eb0518c2ec0c324e215c0c51f2619a5ff89a87e9
Mark-Asuncion <mark.asuncion.000@gmail.com>
2024-01-30 11:38:11 +0800

    rm unused

--- scripts/main_menu.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-var game = preload("res://scene/game.tscn")
-
-func _on_new_game_pressed():
-	get_tree().change_scene_to_file("res://scene/game.tscn")
-
-func _on_exit_pressed():
-	get_tree().quit()
